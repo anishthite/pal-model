b88dad63626a5b02bbed49a630e57f8c5a54327b
mohandodda0 <mohandodda@rocketmail.com>
2020-09-20 22:46:17 -0500

    randomly pick between 10 possible options

--- Retriever.py
+++ Retriever.py
@@ -13,11 +13,12 @@
 import gensim
 import gensim.downloader as api
 import scipy
+import numpy as np
 # DATASET = 'humor_challenge_data/bot_data/qa_total_word2vec.csv'
 # dataset = pd.read_csv(DATASET)
-dataset['word2vec'],''
+# dataset['word2vec'],
 
-dataset['dataset'][np.argmin(dataset[word2vec].apply(lambda x: scipy.spatial.distance.cosine(x, model[query])).values)]
+# dataset['dataset'][np.argmin(dataset[word2vec].apply(lambda x: scipy.spatial.distance.cosine(x, model[query])).values)]
 
 
 
@@ -27,22 +28,28 @@ class Retriever():
     def __init__(self, dataset, tokenized_dataset, word2vec_dataset):
         self.dataset = pd.read_csv(dataset)
         self.tokenized_dataset =  pd.read_csv(tokenized_dataset)
-        self.word2vec_dataset = pd.read_csv(word2vec_dataset)
+        # self.word2vec_dataset = pd.read_csv(word2vec_dataset)
+        self.word2vec_dataset=np.load(word2vec_dataset)['arr_0']
         self.model = api.load("word2vec-google-news-300")
 
 
     def predict(self, query):
-        # print(type(query))
+        print(type(query))
         # query = query['history']
-        # a = sum([model[word] for word in query.split(' ') if word in model.vocab])
-        # mn = np.argmin(self.word2vec_dataset['word2vec'].apply(lambda x: scipy.spatial.distance.cosine(x,model[query])).values)
-        # mn_data = self.dataset.values[mn]
-        # return str(mn_data[0].strip('\n') + ' ' + str(mn_data[1].strip('\n')))
-        if len(query.split(' ')) > 1:
-            dataset_with_query = self.dataset[self.dataset['title'].str.contains(query) | self.dataset['selftext'].str.contains(query)]
-        else:
-            dataset_with_query = self.dataset[self.tokenized_dataset['0'].apply(lambda x : query.lower() in x)]
-        if not dataset_with_query.empty:
+        a = sum([self.model[word] for word in query.split(' ') if word in self.model.vocab])
+        # mn = np.argmin(self.word2vec_dataset['word2vec'].apply(lambda x: scipy.spatial.distance.cosine(x,self.model[query])).values)
+        mn = np.argpartition(np.array([scipy.spatial.distance.cosine(row,a) for row in self.word2vec_dataset]),10)[10]
+        mn_data = self.dataset.values[mn]
+        if not mn_data.empty:
             sample = dataset_with_query.sample(1)
             return str(sample['title'].values[0]).strip('\n') +' '+str(sample['selftext'].values[0]).strip('\n') 
         return "Sorry I don't have a joke about that right now"
+        # return str(mn_data[0].strip('\n') + ' ' + str(mn_data[1].strip('\n')))
+        # if len(query.split(' ')) > 1:
+        #     dataset_with_query = self.dataset[self.dataset['title'].str.contains(query) | self.dataset['selftext'].str.contains(query)]
+        # else:
+        #     dataset_with_query = self.dataset[self.tokenized_dataset['0'].apply(lambda x : query.lower() in x)]
+        # if not dataset_with_query.empty:
+        #     sample = dataset_with_query.sample(1)
+        #     return str(sample['title'].values[0]).strip('\n') +' '+str(sample['selftext'].values[0]).strip('\n') 
+        # return "Sorry I don't have a joke about that right now"
